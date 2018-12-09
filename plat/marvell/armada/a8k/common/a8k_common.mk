fb09e7c8c9b4be72035b38d99ada027948e623c1
Grzegorz Jaszczyk <jaz@semihalf.com>
2018-12-09 23:11:20 +0100

    plat: marvell: armada: make a8k_common.mk and mss_common.mk more generic
    As a preparation for upcoming support for CN9130 platform, which is
    classified as OcteonTx2 product but inherits functionality from a8k,
    allow to use a8k_common.mk and mss_common.mk from outside of
    PLAT_FAMILY_BASE.
    Above is done by introducing BOARD_DIR which needs to be set by each
    platform, before including a8k_common.mk and mss_common.mk. This will
    allow to use mentioned mk files not only for platforms located under
    previously defined PLAT_FAMILY_BASE.
    
    Change-Id: I22356c99bc0419a40ae11e42f37acd50943ea134
    Signed-off-by: Grzegorz Jaszczyk <jaz@semihalf.com>
    Reviewed-on: https://sj1git1.cavium.com/18312
    Reviewed-by: Kostya Porotchkin <kostap@marvell.com>
    Tested-by: Kostya Porotchkin <kostap@marvell.com>

--- plat/marvell/armada/a8k/common/a8k_common.mk
+++ plat/marvell/armada/a8k/common/a8k_common.mk
@@ -7,9 +7,8 @@
 include tools/marvell/doimage/doimage.mk
 
 PLAT_FAMILY		:= a8k
-PLAT_FAMILY_BASE	:= plat/marvell/armada/$(PLAT_FAMILY)
 PLAT_INCLUDE_BASE	:= include/plat/marvell/armada/$(PLAT_FAMILY)
-PLAT_COMMON_BASE	:= $(PLAT_FAMILY_BASE)/common
+PLAT_COMMON_BASE	:= plat/marvell/armada/a8k/common
 MARVELL_DRV_BASE	:= drivers/marvell
 MARVELL_COMMON_BASE	:= plat/marvell/armada/common
 
@@ -45,15 +44,15 @@ MARVELL_GIC_SOURCES	:=	drivers/arm/gic/common/gic_common.c	\
 				drivers/arm/gic/v2/gicv2_helpers.c	\
 				plat/common/plat_gicv2.c
 
-PLAT_INCLUDES		:=	-I$(PLAT_FAMILY_BASE)/$(PLAT)		\
+PLAT_INCLUDES		:=	-I$(BOARD_DIR)				\
 				-I$(PLAT_COMMON_BASE)/include		\
 				-I$(PLAT_INCLUDE_BASE)/common
 
 PLAT_BL_COMMON_SOURCES	:=	$(PLAT_COMMON_BASE)/aarch64/a8k_common.c \
 				drivers/ti/uart/aarch64/16550_console.S
 
-BLE_PORTING_SOURCES	:=	$(PLAT_FAMILY_BASE)/$(PLAT)/board/dram_port.c \
-				$(PLAT_FAMILY_BASE)/$(PLAT)/board/marvell_plat_config.c
+BLE_PORTING_SOURCES	:=	$(BOARD_DIR)/board/dram_port.c \
+				$(BOARD_DIR)/board/marvell_plat_config.c
 
 MARVELL_MOCHI_DRV	+=	$(MARVELL_DRV_BASE)/mochi/cp110_setup.c
 
@@ -80,7 +79,7 @@ MARVELL_DRV		:= 	$(MARVELL_DRV_BASE)/io_win.c	\
 				$(MARVELL_DRV_BASE)/comphy/phy-comphy-cp110.c \
 				$(MARVELL_DRV_BASE)/mc_trustzone/mc_trustzone.c
 
-BL31_PORTING_SOURCES	:=	$(PLAT_FAMILY_BASE)/$(PLAT)/board/marvell_plat_config.c
+BL31_PORTING_SOURCES	:=	$(BOARD_DIR)/board/marvell_plat_config.c
 
 BL31_SOURCES		+=	lib/cpus/aarch64/cortex_a72.S		       \
 				$(PLAT_COMMON_BASE)/aarch64/plat_helpers.S     \
