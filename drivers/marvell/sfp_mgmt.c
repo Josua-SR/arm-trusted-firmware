Gateworks/atf-newport

drivers: marvell: sfp: update board model info to SFP slot
MCP needs board model info for debug purpose in the
SFP slot info passed via shared mem.

Change-Id: Ie6ac2b4ac9b3ee143de4c8e3e08c9ae92d413422
Signed-off-by: Nithya Mani <nithya.mani@cavium.com>
Reviewed-on: https://sj1git1.cavium.com/2902
Tested-by: sa_ip-sw-jenkins
Reviewed-by: Nithya Mani <Nithya.Mani@cavium.com>
Reviewed-by: Alexey Kovalev <Alexey.Kovalev@cavium.com>

drivers/marvell/sfp_mgmt.c
@@ -72,6 +72,11 @@ void sfp_init_shmem(void)
 			/* Assign CGX/LMAC IDs */
 			sh_data->cgx_id = cgx_idx;
 			sh_data->lmac_id = lmac_idx;
+
+			/* Copy the board model */
+			strncpy(sh_data->board_model,
+				plat_octeontx_bcfg->bcfg.board_model,
+				sizeof(sh_data->board_model));
 		}
 	}
 }

Josua-SR/arm-trusted-firmware

drivers/marvell/sfp_mgmt.c
@@ -72,6 +72,11 @@ void sfp_init_shmem(void)
 			/* Assign CGX/LMAC IDs */
 			sh_data->cgx_id = cgx_idx;
 			sh_data->lmac_id = lmac_idx;
+
+			/* Copy the board model */
+			strlcpy(sh_data->board_model,
+				plat_octeontx_bcfg->bcfg.board_model,
+				sizeof(sh_data->board_model));
 		}
 	}
 }